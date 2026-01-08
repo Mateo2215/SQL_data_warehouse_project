1b499ab31dee4059056fcb3b941e7a0781ecdb30
Mateusz Waliszewski <matrixqr@gmail.com>
2026-01-08 21:11:47 +0100

    Update checks.sql

--- scripts/silver/checks.sql
+++ scripts/silver/checks.sql
@@ -20,3 +20,14 @@ WHERE prd_cost IS NULL
 ---- Check for Invalid Date Orders
 SELECT * FROM bronze.crm_prd_info
 WHERE prd_start_dt > prd_end_dt
+
+
+
+--- Check for Invalid Date
+SELECT
+NULLIF (sls_ship_dt,0) AS sls_ship_dt
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt < = 0
+OR LEN(sls_ship_dt) !=8
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 19000101
